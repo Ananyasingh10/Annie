6cf49b4a25366395601697ee20550b47a22e062b
Ananyasingh10 <65295042+Ananyasingh10@users.noreply.github.com>
2020-05-14 13:35:16 +0530

    good
    hi

---
+++ inner.js
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h> my first webpage</h>
+<p> my first para</p>
+<p id="demo"></p>
+<script>
+document.getElementById("demo").innerHTML = 10+20;
+</script>
+</body>
+</html>
---
+++ javah.js
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h> what can javascript do?</h>
+<p id "demo"> javascript change html content</p>
+<button type="button" onclick='document.getElememntById("demo").innerHTML="hello javascript">click me</button>
+</body>
+</html>
